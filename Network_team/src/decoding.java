4deb56bfa36a5ec15f58a25284faf34129cb5110
sts07142 <87844358+sts07142@users.noreply.github.com>
2022-12-12 20:05:21 +0900

    Update decoding.java

--- Network_team/src/decoding.java
+++ Network_team/src/decoding.java
@@ -4,9 +4,7 @@
 
 public class decoding {
 	private static int p=17, q=11, d=23, N=p*q;
-	
-	//인터넷에서 우리의 개인정보는 보안이 되었다. 따라서 다른 유저가 쉽게 접근하는 것을 막기 위해
-	//비밀번호를 RSA 알고리즘을 사용해 보안화했다..
+	//use RSA algorithm to protect information
 	
 	private static int pow(int i, int j, int k) {
 		int l, temp, p=1;
@@ -17,8 +15,6 @@ public class decoding {
 		}
 		return (int)p; 
 	}
-	//encodes는 비밀번호를 보안화한다.
-	//decode는 보안화된 비밀번호를 풀어 원래의 비밀번호를 반환한다. 
 	public static String decode(String arr) {
 		String s;
 		s=decryption(arr, d, N);
