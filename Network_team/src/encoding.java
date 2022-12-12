74b8f1790679a98bb845b27d893ac3f4cc533a49
sts07142 <87844358+sts07142@users.noreply.github.com>
2022-12-12 20:06:00 +0900

    Update encoding.java

--- Network_team/src/encoding.java
+++ Network_team/src/encoding.java
@@ -5,10 +5,7 @@
 public class encoding {
 	private static int p=17, q=11, e=7, N=p*q;
 	public static int size;
-	
-	//인터넷에서 우리의 개인정보는 보안이 되었다. 따라서 다른 유저가 쉽게 접근하는 것을 막기 위해
-	//비밀번호를 RSA 알고리즘을 사용해 보안화했다..
-	
+	//use RSA algorithm to protect information
 	private static int pow(int i, int j, int k) {
 		int l, temp, p=1;
 		for(temp=0; temp<j; temp++) {
@@ -18,7 +15,6 @@ public class encoding {
 		}
 		return (int)p; 
 	}
-	//encodes는 비밀번호를 보안화한다.
 	public static String encode(String pass, int _size) {
 		String s=new String();
 		size=_size;
